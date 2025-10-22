afe0e711d3e9c172024f3179ba31402805e29b06
Stephan Fuchs <fuchsstephan97@gmail.com>
2025-10-22 12:08:05 +0200

    add `qsrc2react.d.ts`

---
+++ exports/qsrc2react.d.ts
@@ -0,0 +1,11 @@
+/**
+ * qsrc2react.d.ts
+ * Typdefinitionen für das npm-Paket @steuchs/qsrc2react
+ */
+
+/**
+ * Übersetzt Quellcode in ein String-Array.
+ * @param codeRaw - der rohe Code, der übersetzt werden soll
+ * @returns ein Array von Strings mit dem übersetzten Code
+ */
+export default function translate(codeRaw: string): string[];
